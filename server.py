b7f4130d4c97bb5399732144354b9372223ca220
Flashyyyyyy <mantere0901@gmail.com>
2024-11-12 22:01:19 +0200

    made client.py
    made client.py and succesfully exchanged data between client and server.

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ import socket
 
 # server sets up a listening socket
 HOST = "127.0.0.1"
-PORT = 1337
+PORT = 13337
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(HOST, PORT)
+    s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     # data is exchanged
